8ec54c4883bf254a2f2686dfdc2a62e6b07d1705
Jennie Robinson Faber <jennie@jenniefaber.com>
2023-10-10 10:55:35 -0400

    Cleanup styles for nav

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,32 +1,33 @@
 <template>
-  <div
-    id="header-wrap"
-    class="flex flex-wrap max-w-screen-xl items-center space-x-2">
+  <div id="header-wrap" class="flex flex-wrap max-w-screen-xl items-center">
     <div
       id="name-box"
-      class="flex max-w-screen-xl items-center space-x-2 w-full lg:w-1/2">
+      class="flex max-w-screen-xl items-center w-full lg:w-1/2">
       <div
         class="py-8 whitespace-nowrap font-display font-black text-4xl tracking-tighter">
         Henry Faber
       </div>
-      <div class="flex items-center space-x-2">
-        <span class="text-5xl">/</span>
+      <div class="flex items-center">
+        <span class="text-5xl px-4">/</span>
         <span class="font-display text-xs lg:text-base font-medium">
           EQUITABLE SPACE - COMMUNITY - VR, VIDEO GAMES + DIGITAL
         </span>
       </div>
     </div>
 
-    <nav>
+    <nav class="w-full lg:w-1/2">
       <ContentNavigation v-slot="{ navigation }">
-        <ul class="list-none p-0 m-0 flex flex-wrap space-x-4">
+        <ul class="list-none pl-4 m-0 grid grid-cols-3 grid-rows-2 gap-x-4">
+          <li></li>
           <li
             class="inline text-base"
             v-for="(link, index) of navigation"
             :key="link._path">
-            <NuxtLink class="text-base hover:text-gray-500" :to="link._path">{{
-              link.title
-            }}</NuxtLink>
+            <NuxtLink
+              class="text-base hover:text-gray-500 uppercase no-underline"
+              :to="link._path"
+              >{{ link.title }}</NuxtLink
+            >
           </li>
         </ul>
       </ContentNavigation>
